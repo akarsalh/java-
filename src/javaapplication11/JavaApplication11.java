4730587df95458f563ccf1b45028058f2c1c0d4b
akarsalh <56086468+akarsalh@users.noreply.github.com>
2019-10-08 12:34:55 -0700

    this is my project for you

---
+++ src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,217 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Akar
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  Scanner akar=new Scanner(System.in);
+//        for (int i = 0,j=1; i < 12; i=i+2,j=j+2) {
+//            System.out.println(i);
+//            System.out.println(j);
+//            
+//        }
+
+
+//        System.out.println("enter base");
+//        int base=akar.nextInt();
+//        System.out.println("enter raise");
+//        int raise=akar.nextInt();
+//        int liset=1;
+//        for (int i = 0; i < raise; i++) {
+//           liset=base*liset;
+//            System.out.println(liset);
+//        }
+
+//        for (int i = 0; i <= 1000; i++) {
+//            if(i%25==0){System.out.println(i+ "chandjaray 25");}
+//            
+//        else if(i%70==0){System.out.println(i+ "chandjaray 70");}
+//
+//    }
+//int base=akar.nextInt();
+//int raise=akar.nextInt();
+//
+//int a=1;
+//while(a<=raise){
+//    a=a*base;
+//
+//        System.out.println(a);
+//}
+       
+//int outer=1;
+//        while (outer<=2) {
+//            
+//          int inner=5;
+//            while (inner<=7) {
+//                System.out.println(outer+ " "+inner);
+//              
+//                inner++;
+//                
+//                
+//            }
+//   
+//            outer++;
+//            
+//            
+//            
+//            
+//        }
+//
+// int outer=1;
+//        while (outer<=10) {
+//          
+//           int iner=1; 
+//            while(iner<=outer) {
+//               
+//                System.out.println(iner+"*"+outer+"="+iner*outer);
+//            iner++;
+//                  int i=1;
+//ئەتوانین هەمان شت بە یەک وایەل بکەی)نمونەی دواتر
+
+//int j=1;
+//while(i<=10){
+//    System.out.println(i*j);
+//
+//i++;
+//j++;
+//}
+//                
+//            
+//            outer++;
+      //  }
+// int i=0;
+//        do {
+//           
+//           int j=0;
+//            do
+//            {
+//                 System.out.println(i+" "+j );  
+//           j++;
+//            } while (j<=2);
+//            
+//           
+//           
+//           i++;  
+//        } while (i<2);
+//        
+//   
+//    int i=1;
+//        do {
+//            int j=1;
+//            do {System.out.println(i*j);
+//               j++; 
+//            } while (j<=10);
+//            i++;
+//        } while (i<=10);
+//        
+//
+//        for (int i = 0; i <= 1; i++) {
+//            for (int j = 0; j <= 2; j++) {
+//                
+//                System.out.println(i+" "+j);
+//            }
+//        }
+//
+//
+//
+//
+//        for (int i = 0; i <= 5; i++) {
+//              
+//            for (int j = 5; j >= i; j--) {
+//                System.out.print( "*");
+////                
+////            }
+//      System.out.println();
+
+
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//                
+//            }
+//            System.out.println();
+//        }
+//        for (int a = 0; a < 4; a++) {
+//            for (int b = 0; b > a; b--) {
+//                System.out.println("*");
+//            }
+//            System.out.println();
+//        }
+
+//loop:  for (int i = 0; i <= 10; i++) {;;;
+//loop2:    for (int j = 0; j <= 10; j++) {
+//                
+//                System.out.print(j);
+//                 if(j==6) break loop;
+//            
+//            }
+//            
+//            
+//            System.out.println();
+//        }
+
+//int num= 763;
+//double x = 658.75;
+//String str= "Java Program.";
+//
+//System.out.printf("%5d%7.2f%15s%n",num, x, str);
+//System.out.printf("%s%7d%9.2f%n",str, num, x);
+//System.out.printf("%8.2f%7d%15s%n",x, num, str);
+//System.out.printf("num= %5d%n", num);
+//System.out.printf("x = %10.2f%n", x);
+//System.out.printf("str= %15s%n", str);
+//System.out.printf("%10s%7d%n","Program No.", 4);
+
+
+
+//int a=10;
+//int b=20;
+//int r=0;
+//r=akar(a,b);
+// System.out.println(r);
+//    }
+//
+// public static int akar(int a,int b){
+//
+//return a+b;
+    
+
+ }
+}
+
+
+      
+    
+    
+   
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+
